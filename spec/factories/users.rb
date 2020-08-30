e7a054b208d8474baebad01d1596abe20330209d
hironori0723 <604849hiro.s@gmail.com>
2020-08-30 17:01:16 +0900

    toppageを作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    name { "MyString" }
+    email { "MyString" }
+    password_digest { "MyString" }
+    remember_digest { "MyString" }
+    reset_digest { "MyString" }
+  end
+end
